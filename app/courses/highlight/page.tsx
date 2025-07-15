5c55acf912d645091bcf39866fcf2220891b8776
Ange-Pascal <angeakoumoua@gmail.com>
2025-07-16 01:33:39 +0200

    reconfiguration du module course

---
+++ app/courses/highlight/page.tsx
@@ -0,0 +1,17 @@
+export default function CourseHighlight() {
+  return (
+    <div>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo nihil
+      eos fugiat aspernatur, natus sint nesciunt dolorem doloribus mollitia,
+      facere consequuntur assumenda minus sed, numquam nam? Voluptatum eum
+      provident accusamus laborum at ea adipisci sint maxime, dolore iure
+      doloribus ad, eveniet dolor quo vitae quam ipsa error quaerat quod
+      necessitatibus. Architecto enim eius animi non assumenda possimus quis,
+      fuga vel officia nihil fugit magni sequi sit dolorum vitae veniam
+      accusantium, commodi officiis necessitatibus esse, reprehenderit in. Ab
+      soluta incidunt eligendi ratione voluptatibus? Ea, incidunt obcaecati
+      ratione natus debitis quaerat aliquam amet cum unde, ipsam totam rerum,
+      rem odit sed explicabo!
+    </div>
+  );
+}
---
+++ app/courses/update/page.tsx
@@ -0,0 +1,17 @@
+export default function CourseUpdate() {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime architecto
+      nemo quam animi a quisquam cumque ab delectus, ex minus perspiciatis quos
+      ipsam. Deleniti earum nesciunt asperiores saepe corporis unde iusto
+      necessitatibus! Harum dolorem atque aspernatur minus. Esse voluptatem
+      adipisci non est labore tempora mollitia aliquid numquam, deleniti modi
+      distinctio eaque repudiandae fugiat atque officia. Laboriosam hic, et
+      incidunt soluta officiis in possimus! Sint maiores asperiores, quisquam
+      tenetur dolore ab voluptates porro debitis soluta. Quod, exercitationem
+      amet! Tenetur et commodi consectetur architecto pariatur quibusdam magni
+      repellendus dicta tempore soluta voluptas aliquam, atque a eos odit
+      repellat veritatis ullam illo corrupti.
+    </div>
+  );
+}
