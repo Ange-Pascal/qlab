6b28611ea64ebb677674074e7b4662a360637d68
Ange-Pascal <angeakoumoua@gmail.com>
2025-08-14 14:35:04 +0000

    wizard module config

--- components/courses/AddCourseModule.tsx
+++ components/courses/AddCourseModule.tsx
@@ -1,8 +1,3 @@
-export default function AddCourseModule(){
-    return (
-        <div className="p-10">
-            <h1 className="text-3xl text-red-700"> Add Course Module Page</h1>
-        </div>
-    )
-    
+export default function AddCourseModulePage(){
+    return 
 }
